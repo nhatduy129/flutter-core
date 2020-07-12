ecf4f2c2d80bc372417078625b3f2c568335131a
Duy Nguyen <nguyenvunhatduy@gmail.com>
2020-07-12 20:18:54 +0800

    row

--- f01_myapp/lib/main.dart
+++ f01_myapp/lib/main.dart
@@ -14,13 +14,31 @@ class Home extends StatelessWidget {
         title: Text('hello Duy'),
         centerTitle: true,
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            print('lskdf');
-          },
-          child: Text('click me'),
-        )
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: Container(
+                padding: EdgeInsets.all(20.0),
+                color: Colors.orange[600],
+                child: Text('1')
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+                padding: EdgeInsets.all(20.0),
+                color: Colors.red[600],
+                child: Text('2')
+            ),
+          ),
+          Container(
+              padding: EdgeInsets.all(20.0),
+              color: Colors.blue[600],
+              child: Text('3')
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Text('click a'),

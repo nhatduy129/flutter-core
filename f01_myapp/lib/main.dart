0ec56cf3229541e44a2754fdac396b51e966334b
Duy Nguyen <nguyenvunhatduy@gmail.com>
2020-07-12 15:54:44 +0800

    demo raisedButton

--- f01_myapp/lib/main.dart
+++ f01_myapp/lib/main.dart
@@ -2,17 +2,29 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text('hello Duy'),
         centerTitle: true,
       ),
       body: Center(
-        child: Text('my first app'),
+        child: RaisedButton(
+          onPressed: () {
+            print('lskdf');
+          },
+          child: Text('click me'),
+        )
       ),
       floatingActionButton: FloatingActionButton(
-        child: Text('click'),
+        child: Text('click a'),
       ),
-    )
-  ));
+    );
+  }
 }
